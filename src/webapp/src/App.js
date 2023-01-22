2409a69e90af9a38f83ae41dc3740209b7a73c2c
walidoow <walidoo_2002@hotmail.com>
2023-01-22 02:22:18 -0500

    Updated sidebar

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/sidebar/Sidebar';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="">
-      <Navbar />
+      <Sidebar />
     </div>
   );
 }
