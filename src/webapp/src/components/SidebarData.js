1efb936afa10dacb9c5a64373df2c30367cdc070
Nour <s_nour@videotron.ca>
2023-01-22 00:49:17 -0500

    Creation of component files : sidebar, sidebarData,  and subMenu

---
+++ src/webapp/src/components/SidebarData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as Falcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import * as IoIcons from 'react-icons/io'
+
+export const SidebarData = [
+    title: 'Overview',
+    path:'/overview',
+    icon: <AiIcons.AiFillHome/>,
+
+]
