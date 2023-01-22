4334bd6606152b26437e5cdcb9e37844293654eb
Nour <s_nour@videotron.ca>
2023-01-22 02:27:49 -0500

    update sidebardata and sidebar

--- src/webapp/src/components/SidebarData.js
+++ src/webapp/src/components/SidebarData.js
@@ -2,10 +2,61 @@ import React from 'react'
 import * as Falcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import * as IoIcons from 'react-icons/io'
+import * as RiIcons from 'react-icons/ri'
 
 export const SidebarData = [
-    title: 'Overview',
-    path:'/overview',
-    icon: <AiIcons.AiFillHome/>,
+    {
+        title: 'Overview',
+        path: '/overview',
+        icon: <AiIcons.AiFillHome/>,
+        iconClosed: <RiIcons.RiArroDownSFill/>,
+        iconOpened: <RiIcons.RiArrowUpSFill/>,
+        subNav:[
+        {
+            title: 'Users',
+            path: '/overview/users',
+            icon: <IoIcons.IoIosPaper/>,
+        },
+        {
+            title: 'Revenue',
+            path: '/overview/revenue',
+            icon: <IoIcons.IoIosPaper/>,
+        },
+
+        ]
+
+    },
+    {
+        title: 'Reports',
+        path: '/reports',
+        icon: <AiIcons.AiFillHome/>,
+        iconClosed: <RiIcons.RiArroDownSFill/>,
+        iconOpened: <RiIcons.RiArrowUpSFill/>,
+        subNav:[
+        {
+            title: 'Reports',
+            path: '/reports/reports1',
+            icon: <IoIcons.IoIosPaper/>,
+        },
+        {
+            title: 'Reports 2',
+            path: '/reports/reports1',
+            icon: <IoIcons.IoIosPaper/>,
+        },
+        {
+            title: 'Reports 3',
+            path: '/reports/reports1',
+            icon: <IoIcons.IoIosPaper/>,
+        },
+
+        ]
+
+    },
+    {
+        title:'Products',
+        path:'/products',
+        icon:<FaIcons.FaCartPlus/>
+    }
+   
 
 ]
