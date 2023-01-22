4888c16918c7050f3cd03f160516d7650f9bb35f
kingadamo <78397077+kingadamo@users.noreply.github.com>
2023-01-21 23:39:36 -0500

    Separation exchange and stock socket
    changed path to avoid confusion between HTTP paths and WS paths

--- src/main/java/com/conuhacks/nbc/messagingwebsocket/ExchangeSocketHandler.java
+++ src/main/java/com/conuhacks/nbc/messagingwebsocket/ExchangeSocketHandler.java
@@ -7,12 +7,14 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 
-public class SocketTextHandler extends TextWebSocketHandler {
-
+public class ExchangeSocketHandler extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
         String payload = message.getPayload();
         JSONObject jsonObject = new JSONObject(payload);
-        session.sendMessage(new TextMessage("Hi " + jsonObject.get("user") + " how may we help you?"));
+
+
+        session.sendMessage(new TextMessage("EXCHANGE " + jsonObject.get("user") + " how may we help you?"));
     }
+
 }
--- src/main/java/com/conuhacks/nbc/messagingwebsocket/WebSocketConfig.java
+++ src/main/java/com/conuhacks/nbc/messagingwebsocket/WebSocketConfig.java
@@ -1,14 +1,18 @@
 package com.conuhacks.nbc.messagingwebsocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketTextHandler(), "/user");
+        registry.addHandler(new StockPriceSocketHandler(), "/ws/stock");
+        registry.addHandler(new ExchangeSocketHandler(), "/ws/exchange");
     }
+
 
 }
